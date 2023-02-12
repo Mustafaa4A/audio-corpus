aa10c8433022491c1cb7e00959b20f1145769537
Hafsa Abdukadir <hafzaabdulkhadir@gmail.com>
2023-02-12 10:14:21 -0800

    adding navBAr

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import ResponsiveNav from "./components/nav";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <div>
+      <ResponsiveNav />
       <Routes>
         <Route path="/" element={<h1>dfv</h1>} />
         <Route path="/Login" element={<SignIn />} />
