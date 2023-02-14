7d1c460eec6d8882e90e18d4388290e2ce7cf3b0
Mustafaa4a <musdhafa42@gmail.com>
2023-02-14 13:23:34 +0300

    Adding recording factionality

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -1,6 +1,8 @@
-import { ArrowBack,  CropSquare, KeyboardDoubleArrowRight, KeyboardVoice } from '@mui/icons-material'
+import { CropSquare, KeyboardDoubleArrowRight, KeyboardVoice, PlayArrow, Replay, Stop } from '@mui/icons-material'
 import { Modal, Box, Button, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useReactMediaRecorder } from 'react-media-recorder';
+import useSound from 'use-sound';
 import AudioControl from '../components/AudioControl'
 import BackArrow from '../components/BackArrow';
 import CustomBtn from '../components/CustomBtn';
@@ -9,16 +11,49 @@ import Wrap from '../components/Wrap'
 import data from '../data';
 
 const Contribute = () => {
-  const [open, setOpen] = useState(false);
+  const { startRecording, stopRecording, pauseRecording, mediaBlobUrl, clearBlobUrl } = useReactMediaRecorder({
+    video: false,
+    audio: true,
+    echoCancellation: true
+  });
+  const [play, { stop }] = useSound(mediaBlobUrl);
+  const audio = new Audio(mediaBlobUrl);
   const [recording, setRecording] = useState(false);
+  const [playing, setPlaying] = useState(false);
+
+  const [open, setOpen] = useState(false);
   const [text, setText] = useState();
 
   const handleModel = () => setOpen(prev => !prev);
-  const handleRecord = () => setRecording(prev => !prev);
+  const start = () => {
+    startRecording();
+    setRecording(prev => !prev);
+  };
+
+  const stopRecord = () => {
+    pauseRecording();
+    stopRecording();
+    setRecording(prev => !prev);
+  };
+
+  const playAudio = () => {
+    setPlaying(true);
+    audio.play();
+  }
+
+  const pauseAudio = () => {
+    setPlaying(false);
+    audio.pause();
+  }
+
+  const clear = () => {
+    pauseAudio();
+    clearBlobUrl();
+  }
 
   const generateText = () => {
     const id = parseInt(Math.random() * 10);
-    setText(data[id].text);
+    setText(data[1].text);
   }
 
   useEffect(() => {
@@ -36,6 +71,9 @@ const Contribute = () => {
         onClose={handleModel}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        sx={{
+          bgcolor: 'blur',
+        }}
       >
         <>
           <BackArrow onClick={ handleModel } />
@@ -61,27 +99,51 @@ const Contribute = () => {
             }
             
             <TextDisplay> { text } </TextDisplay>
-
             {
-              (!recording) ? (
-                <AudioControl onClick={handleRecord}>
-                  <KeyboardVoice sx={{
-                  fontSize: 30,
-                  color:'red'
-                 }} />
-              </AudioControl>
+              (!mediaBlobUrl) ? (
+                (!recording) ? (
+                  <AudioControl onClick={start}>
+                    <KeyboardVoice sx={{
+                      fontSize: 30,
+                      color:'red'
+                    }} />
+                  </AudioControl>
+                ) : (
+                  <AudioControl onClick={stopRecord}>
+                    <CropSquare sx={{
+                      fontSize: 30,
+                      color:'red'
+                    }} />
+                  </AudioControl>
+                )
               ) : (
-                <AudioControl onClick={handleRecord}>
-                  <CropSquare sx={{
-                  fontSize: 30,
-                  color:'red'
-            }} />
-            </AudioControl>
+                <Box sx={{ display: 'flex', gap: 5 }}>
+                <AudioControl disabled={playing} onClick={clear} >
+                  <Replay sx={{
+                      fontSize: 40,
+                      color:'red'
+                    }} />
+                </AudioControl>
+                  {
+                    (playing) ? (
+                      <AudioControl onClick={pauseAudio}>
+                        <Stop  sx={{
+                        fontSize: 40,
+                          color:'red'
+                        }} /> 
+                      </AudioControl>
+                        ) : (
+                      <AudioControl  onClick={playAudio}>
+                        <PlayArrow  sx={{
+                        fontSize: 40,
+                          color:'green'
+                            }} /> 
+                      </AudioControl>
+                      ) 
+                    }
+              </Box>
               )
             }
-
-          
-            
           </Box>
 
           <Box sx={{
@@ -95,11 +157,10 @@ const Contribute = () => {
             <CustomBtn color='success' onClick={generateText}>
               Skip  <KeyboardDoubleArrowRight />
            </CustomBtn>
-            <CustomBtn disabled>
+            <CustomBtn disabled={!mediaBlobUrl}>
               Submit
             </CustomBtn>
           </Box>
-          
         </>
       </Modal>
     </Wrap>
